4fc1fc06e2c36b14c63934255c3cabad92abc54e
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-29 11:55:19 -0300

    aula

--- KakoFlix/src/Pages/Home.jsx
+++ KakoFlix/src/Pages/Home.jsx
@@ -2,17 +2,34 @@ import data from '../../artigos.json'
 
 function Home() {
     return ( 
-        <>      {
+        <div className='grid-grid-cols-3'>      {
         data.map(
             (filme, index) => (
+                <>
                 <div className='card' key={index}>
                     <h1>{filme.title}</h1>
                     <img src={filme.image}/>
                  </div>
+                 <div className='tags'>
+                    {
+                        filme.tags.map( tag => (
+                            <span className="bg-pink-800 text-white p-1 m-1" key={tag}>{tag}</span>
+                        ))
+                    }
+
+                 </div> 
+                 <div className='text'>
+                    {
+                        filme.text.map(text =>(
+                            <span className='p-1 m-1' key={text}>{text}</span>
+                        ))
+                    }
+                 </div>
+                 </>
                 )
             )
         } 
-        </>   
+        </div>   
      );
 }
 
